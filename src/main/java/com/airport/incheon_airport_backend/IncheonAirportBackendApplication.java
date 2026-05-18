2186590adacebd4efbc6cdc3d01d9ca9d836366d
hoyoung <hoyoung0330@gmail.com>
2026-05-18 12:41:10 +0900

    feat: Spring Boot 프로젝트 초기 셋업

---
+++ src/main/java/com/airport/incheon_airport_backend/IncheonAirportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.airport.incheon_airport_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncheonAirportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncheonAirportBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airport/incheon_airport_backend/IncheonAirportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airport.incheon_airport_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncheonAirportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
